942b1fdfaee57938640a5aa86dbb0f3cb18501cb
scayez <162686801+scayez@users.noreply.github.com>
2026-05-05 08:48:34 +0200

    Add files via upload

---
+++ exercices9_fichiers_correction/exercice2/exercice923.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtGui import QPixmap #<-------------
+
+
+# Chargement de l'interface créée avec Qt Designer
+base_dir = os.path.dirname(os.path.abspath(__file__))
+qtCreatorFile = os.path.join(base_dir, "ui921.ui")
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        # Le menu "Ouvrir" sera connecté à la fonction ouvrir_fichiers dans l'étape suivante
+        self.actionOuvrir.triggered.connect(self.ouvrir_fichiers)
+
+
+    def ouvrir_fichiers(self):
+       file_name, _ = QFileDialog.getOpenFileName(self, "Ouvrir un ou plusieurs fichiers", "", "Images (*.png *.jpg *.jpeg)") #<-------------
+       if file_name:
+           print("Fichiers sélectionnés :", type(file_name))
+           # Affichage de la première image de la liste
+           pixmap = QPixmap(file_name)#<-------------
+           self.label_image.setPixmap(pixmap)#<-------------
+           self.label_image.setScaledContents(True)#<-------------        
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
